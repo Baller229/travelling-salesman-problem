c360220e285fabe62aef127e610a6ca682db5a23
Patrik Fusek <patrik.fusek2@gmail.com>
2022-11-01 21:50:11 +0100

    create initial structure with empty classes

---
+++ travellingSalesmanProblem/src/main/java/City.java
@@ -0,0 +1,13 @@
+public class City
+{
+   String position;
+   int X;
+   int Y;
+
+   public City(String position, int X, int Y)
+   {
+      this.position = position;
+      this.X = X;
+      this.Y = Y;
+   }
+}
